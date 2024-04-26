cdb3738a14f9b8be851ced78046cb4f80c48d8a5
Andrey_Mkoyan <and.mkoyan07@gmail.com>
2024-04-26 10:34:48 +0400

    Models project complete. Repository project created with Account repository.

---
+++ BlogLab.Repasitory/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using BlogLab.Models.Account;
+namespace BlogLab.Repasitory
+{
+    public interface IAccountRepository
+    {
+        public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user,
+            CancellationToken cancellationToken);
+        public Task<ApplicationUserIdentity> GetByUsernameAsync(string normalizedUsername,
+            CancellationToken cancellationToken);
+
+    }
+}
